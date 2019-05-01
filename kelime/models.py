55229e5ba86a224af4b8eb4efed61b4d53ccddbe
İsmail Sefa <47992291+SefaAkdeniz@users.noreply.github.com>
2019-05-01 22:22:42 +0300

    Part of Learning Completed

--- kelime/models.py
+++ kelime/models.py
@@ -12,5 +12,5 @@ class Kelime(models.Model):
 class KelimeBilgi(models.Model):
     word=models.ForeignKey(Kelime,on_delete = models.CASCADE,verbose_name ="Kelime")
     user=models.ForeignKey("auth.User",on_delete = models.CASCADE,verbose_name ="Kullanıcı")
-    date = models.DateTimeField(verbose_name= "Sorulacak Tarih")
+    date = models.DateTimeField(blank=True,null=True,verbose_name= "Sorulacak Tarih")
     level=models.IntegerField(verbose_name="Seviyesi")
--- kelime/views.py
+++ kelime/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,redirect
 from kelime.models import Kelime,KelimeBilgi
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -8,13 +9,38 @@ def index(request):
 
 @login_required(login_url = "user:login")
 def question(request):
+    
+    cevap=request.GET.get("answer")
+    soru=request.GET.get("soru")  
+    
+    if cevap:
+        dogru=Kelime.objects.filter(engWord=soru).first()
+        
+        print(dogru)
+        print(cevap)
+        print(soru)
+        
 
+        
+        if dogru.trWord.upper()==cevap.upper():
+            messages.success(request,"Tebrikler Doğru Bildiniz..")
+            Kayit=KelimeBilgi(user=request.user,word=dogru,level=1)
+            Kayit.save()
+        else:
+            messages.info(request,"Maalesef Yanlış Yaptınız..")
+    
     control=True
-
     while control:
         kelime = Kelime.objects.order_by("?").first()
         count = KelimeBilgi.objects.filter(word=kelime,user=request.user).count()
         if(count==0):
             control=False
+        
+        kelimeSayisi=Kelime.objects.all().count()
+        kayitliVeri=KelimeBilgi.objects.filter(user=request.user).count()
+
+        if kelimeSayisi==kayitliVeri:
+            messages.info(request,"Kelime Kalmadı")
+            return redirect("index")
 
     return render(request,"question.html",{"kelime":kelime})
